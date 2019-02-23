7e817029fc0df3b1715d679699ad54820abc4ce8
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-23 14:08:30 -0700

    Add test with mock data

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -6,4 +6,9 @@ RSpec.feature "Home Page", type: :feature do
     visit '/'
     expect(page).to have_content('Riptide')
   end
+
+  it 'shows some surf data' do
+    visit '/'
+    expect(page).to have_content('Wind Speed: 10')
+  end
 end
