afe14a4ce5f39ae16a09f41c37cee376b6bfc8a8
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-23 18:57:08 -0700

    Build out SurfDataFacade to pull relevant information from response

--- app/facades/surf_data_facade.rb
+++ app/facades/surf_data_facade.rb
@@ -1,6 +1,30 @@
 class SurfDataFacade
 
+  def timestamp
+    data[:timestamp]
+  end
+
+  def wind
+    data[:wind]
+  end
+
+  def conditions
+    data[:condition]
+  end
+
+  def charts
+    data[:charts]
+  end
+
+  def swell
+    data[:swell]
+  end
+
   private
+
+  def data
+    service.get_json
+  end
 
   def service
     SeaweedService.new
