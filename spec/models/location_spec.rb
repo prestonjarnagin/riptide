c319e60381e0a51abcd20a51552390ca3b58c276
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-24 18:51:42 -0700

    Add validations on location to spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-
+  describe 'Valiadtions' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:region) }
+  end
 end
