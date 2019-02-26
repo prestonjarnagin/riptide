8e93e21ce0d0c8b0fda0d2c95fb8d9249147823e
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-26 14:37:53 -0700

    Update expectations to comply with real data

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.feature "Home Page", type: :feature do
 
   it 'shows name of application' do
+    Location.create(id: 1, name: "Newquay - Fistral North", region: "UK + Ireland")
+
     visit '/'
     expect(page).to have_content('Riptide')
   end
 
   it 'shows some surf data' do
+    Location.create(id: 1, name: "Newquay - Fistral North", region: "UK + Ireland")
+
     visit '/'
-    expect(page).to have_content('Wind Speed: 10')
+    expect(page).to have_content('Wind Speed:')
   end
 end
