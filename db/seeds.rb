835e7d82b5f2840d0a06573e30c39970b9562f53
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-26 16:44:48 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if Rails.env.production?
-  seed = ActionController::Base.helpers.asset_path('surf_locations.json')
-  json = JSON.parse(File.read(seed))
-  json.each do |record|
-    id = record.keys[0]
-    location_name = record.values[0]['location']
-    location_region = record.values[0]['region']
-    Location.create!(id: id, location_name: location_name, location_region: location_region)
-  end
-else
-  seed = './app/assets/documents/surf_locations.json'
-  json = JSON.parse(File.read(seed))
-  json.each do |record|
-    id = record.keys[0]
-    location_name = record.values[0]['location']
-    location_region = record.values[0]['region']
-    Location.create!(id: id, location_name: location_name, location_region: location_region)
+seed = './public/assets/surf_locations.json'
+json = JSON.parse(File.read(seed))
+json.each do |record|
+  id = record.keys[0]
+  name = record.values[0]['location']
+  region = record.values[0]['region']
+  begin
+    p "Location #{id} Created!"
+    Location.create(id: id, name: name, region: region)
+  rescue
+    p "Error Creating Location #{id}"
   end
 end
