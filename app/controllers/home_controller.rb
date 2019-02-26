ce8cdc6f0f3e6d37970c9d1ff93012525b2e6192
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-26 13:32:09 -0700

    Repurpose show to return surf data for a specific location

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
 
   def show
+    # Returns a new facade with information for
+    # the requested location
+    payload = SurfDataFacade.new(params[:id])
+    render json: SurfDataSerializer.new(payload)
+  end
+
   def index
     @facade = SurfDataFacade.new(1)
   end
