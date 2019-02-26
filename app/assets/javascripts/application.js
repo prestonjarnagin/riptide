18a78b4e7c6a435fe8f028994454913431bb0e15
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-26 14:33:59 -0700

    fill in js skeleton with functions to update info based on response

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,8 +26,8 @@ function updateInfo(id){
     conditions = attrs["conditions"]
     waves = attrs["swell"]
     wind = attrs["wind"]
-    debugger;
 
+    updateLocationLabel(attrs['location_name'], attrs['region_name'])
     updateTimestamp(timestamp)
     updateConditions(conditions)
     updateWaves(waves)
@@ -36,16 +36,26 @@ function updateInfo(id){
   .catch(error => console.error(error))
 }
 
+function updateLocationLabel(locationName, regionName){
+  document.getElementById('location-name').innerHTML = locationName
+  document.getElementById('subregion-name').innerHTML = regionName
+}
+
 function updateTimestamp(unixTimestamp){
   date = new Date(unixTimestamp*1000)
   document.getElementById('time-value').innerHTML = (date)
 }
 function updateConditions(conditionsData){
-
+  document.getElementById('temp').innerHTML = conditionsData["temperature"]
+  document.getElementById('pressure').innerHTML = `${conditionsData["pressure"]} ${conditionsData["unitPressure"]}`
 }
 function updateWaves(waveData){
+  document.getElementById('min-breaking-height').innerHTML = waveData["minBreakingHeight"]
+  document.getElementById('max-breaking-height').innerHTML = waveData["maxBreakingHeight"]
 
 }
 function updateWind(windData){
-
+  document.getElementById('wind-speed').innerHTML = windData["speed"]
+  document.getElementById('direction').innerHTML = windData["direction"]
+  document.getElementById('wind-chill').innerHTML = windData["chill"]
 }
