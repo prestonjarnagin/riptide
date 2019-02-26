ecfc5db1bd1d5cd6806fab0537d84f701dfbf452
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-26 13:33:50 -0700

    Add spec for data scraper

--- spec/services/seaweed_data_scraper_spec.rb
+++ spec/services/seaweed_data_scraper_spec.rb
@@ -7,11 +7,30 @@ RSpec.describe SeaweedDataScraper do
   end
 
   it 'retrieves data' do
-    id = Location.first.id
+      loc = Location.create(id: 1, name: "Newquay - Fistral North", region: "UK + Ireland")
 
-    response = described_class.get_or_create_document(id)
+      response = described_class.get_or_create_document(loc.id)
+      expect(response).to be_kind_of(String)
+      expect(response).to include("Surf Report")
+  end
+
+  it 'stores data in redis' do
+    loc = Location.create(id: 1, name: "Newquay - Fistral North", region: "UK + Ireland")
+    # $redis.del(loc.id)
+
+    described_class.get_or_create_document(loc.id)
 
     # expect($redis.get(loc.id)).to_not be_nil
   end
 
+  it 'returns a hash with information' do
+    loc = Location.create(id: 1, name: "Newquay - Fistral North", region: "UK + Ireland")
+    # $redis.del(loc.id)
+
+    actual = described_class.json(loc.id)
+
+    expect(actual).to be_kind_of(Hash)
+    expect(actual[:timestamp]).to be_kind_of(Integer)
+    expect(actual).to be_kind_of(Hash)
+  end
 end
