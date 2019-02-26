05216bbcb055823a0561261c5e39e1c2638683bf
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-26 16:32:28 -0700

    Update seeds to read from JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.production?
+  seed = ActionController::Base.helpers.asset_path('surf_locations.json')
+  json = JSON.parse(File.read(seed))
+  json.each do |record|
+    id = record.keys[0]
+    location_name = record.values[0]['location']
+    location_region = record.values[0]['region']
+    Location.create!(id: id, location_name: location_name, location_region: location_region)
+  end
+else
+  seed = './app/assets/documents/surf_locations.json'
+  json = JSON.parse(File.read(seed))
+  json.each do |record|
+    id = record.keys[0]
+    location_name = record.values[0]['location']
+    location_region = record.values[0]['region']
+    Location.create!(id: id, location_name: location_name, location_region: location_region)
+  end
+end
